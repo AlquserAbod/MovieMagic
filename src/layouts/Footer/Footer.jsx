90360661270fe1d962abdd8583391c8d3369057e
Alquser Abdullah <96134552+AlquserAbod@users.noreply.github.com>
2024-09-07 10:09:43 +0300

    add copyright to https://alquser.vercel.app/

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -4,11 +4,12 @@ import './Footer.css';
 function Footer() {
   return (
    <footer className="footer">
-        <div className="container">
-            <p className='copryte'>
+            <p className='copyright'>
                 moviemagice using <a href="https://www.themoviedb.org/">themoviedb</a> for get movies
             </p>
-        </div>
+            <p className='copyright'>
+            this website is developed from <a href="https://alquser.vercel.app/">Alquser Abdullah</a>
+            </p>
    </footer>
   )
 }
